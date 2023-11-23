d732b2c361bc9d02ce3cd1f99135e5269fa0d82f
Madhura Gore <madhugore1@gmail.com>
2023-11-22 20:35:07 -0800

    Laying the foundation - Basic react code, functional components

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I'm an h1 tag"), React.createElement("h2", {}, "I'm an h2 tag")]
-    )
+// React.createElement -> JS obj -> HTML Element
+const heading = React.createElement("div", { id: "heading" }, "Namaste React");
+
+console.log(heading);
+
+// JSX (transpiled before it reaches JS Enginer) => Done by Parcel -> Babel
+// JSX => Babel transpiles it to React.createElement -> JS obj -> HTML Element
+const jsxHeading = <h1>Namaste React from JSX!</h1>;
+
+console.log(jsxHeading);
+
+const Title = () => <h1>Namaste React Title!</h1>;
+
+const titleElem = <h2>Title Element</h2>;
+
+// Component Composition -> Combining multiple components
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    {Title()}
+    {/* JSX sanitizes the data before executing JS in the browser to take care of cross site scripting (XSS)attacks */}
+    {titleElem}
+    <h1>Namaste React from Functional Component!</h1>
+  </div>
 );
-
-console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
